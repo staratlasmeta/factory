abff6ba4c76941a23faab2f174ce08ba3659078f
bravetarget <bravetarget@gmail.com>
2021-06-01 23:18:28 -0600

    feat: Adds faction enlistment transaction function CU-w77x3r

---
+++ src/factions.ts
@@ -0,0 +1,46 @@
+import {
+  PublicKey, SYSVAR_RENT_PUBKEY, Transaction, TransactionInstruction,
+} from '@solana/web3.js';
+import { longToByteArray } from './util';
+
+const FACTION_PREFIX = 'FACTION_ENLISTMENT';
+
+/**
+ * Get player faction account
+ */
+export async function getPlayerFactionAccount(
+  playerPublicKey: PublicKey, programId: PublicKey,
+): Promise<[PublicKey, number]> {
+  return PublicKey.findProgramAddress([
+    Buffer.from(FACTION_PREFIX, 'utf8'),
+    playerPublicKey.toBuffer(),
+  ], programId);
+}
+
+/**
+ * Enlist player to faction
+ */
+export async function enlistPlayer(
+  factionID: number,
+  payerKey: PublicKey = null,
+  enlistInfoKey: PublicKey = null,
+  programId: PublicKey = null,
+) {
+  const derivedAddressData = await getPlayerFactionAccount(payerKey, programId);
+
+  const systemProgramPubKey = new PublicKey('11111111111111111111111111111111');
+  // Create Associated Player Faction Account
+  const instruction = new TransactionInstruction({
+    keys: [{ pubkey: payerKey, isSigner: true, isWritable: true },
+      { pubkey: derivedAddressData[0], isSigner: false, isWritable: true },
+      { pubkey: enlistInfoKey, isSigner: false, isWritable: true },
+      { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+      { pubkey: systemProgramPubKey, isSigner: false, isWritable: false }],
+    programId,
+    data: Buffer.from([0, ...longToByteArray(factionID)]),
+  });
+
+  const transaction = new Transaction().add(instruction);
+
+  return transaction;
+}
