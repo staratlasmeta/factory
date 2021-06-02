5e579b7d034cb990320641341a64e31c2f0ef338
bravetarget <bravetarget@gmail.com>
2021-06-02 13:43:06 -0600

    feat: Updated faction enlistment tx function CU-w77x3r

--- src/factions.ts
+++ src/factions.ts
@@ -4,6 +4,7 @@ import {
 import { longToByteArray } from './util';
 
 const FACTION_PREFIX = 'FACTION_ENLISTMENT';
+const ENLIST_INFO_SEED = 'ENLIST_INFO';
 
 /**
  * Get player faction account
@@ -18,21 +19,30 @@ export async function getPlayerFactionAccount(
 }
 
 /**
+ * Get enlist info address
+ */
+async function getEnlistInfoAccount(programId: PublicKey): Promise<[PublicKey, number]> {
+  return PublicKey.findProgramAddress([
+    Buffer.from(ENLIST_INFO_SEED, 'utf8')
+  ], programId);
+}
+
+/**
  * Enlist player to faction
  */
 export async function enlistToFaction(
   factionID: number,
   playerKey: PublicKey = null,
-  enlistInfoKey: PublicKey = null,
   programId: PublicKey = null,
 ): Promise<Transaction> {
-  const derivedAddressData = await getPlayerFactionAccount(playerKey, programId);
+  const [playerFactionPDA] = await getPlayerFactionAccount(playerKey, programId);
+  const [enlistInfoKey] = await getEnlistInfoAccount(programId);
 
   const systemProgramPubKey = new PublicKey('11111111111111111111111111111111');
   // Create Associated Player Faction Account
   const instruction = new TransactionInstruction({
     keys: [{ pubkey: playerKey, isSigner: true, isWritable: true },
-      { pubkey: derivedAddressData[0], isSigner: false, isWritable: true },
+      { pubkey: playerFactionPDA, isSigner: false, isWritable: true },
       { pubkey: enlistInfoKey, isSigner: false, isWritable: true },
       { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
       { pubkey: systemProgramPubKey, isSigner: false, isWritable: false }],
