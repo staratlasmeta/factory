adb39423c408377ddf30b9d44e0de743ce363012
bravetarget <bravetarget@gmail.com>
2021-06-01 23:18:28 -0600

    feat: Adds faction enlistment transaction function CU-w77x3r

---
+++ src/util/helpers.ts
@@ -0,0 +1,37 @@
+export function byteArrayToString(array: number[]): string {
+  return String.fromCharCode(...array);
+}
+
+export function stringToByteArray(str: string, length: number): any[] {
+  if (str.length > length) {
+    console.log(`Unable to get byte array, length greater than ${length}`);
+    return null;
+  }
+  let byteArray = [];
+  for (let i = 0; i < length; i += 1) {
+    let code = 32;
+    if (i < str.length) {
+      code = str.charCodeAt(i);
+    }
+    byteArray = byteArray.concat([code]);
+  }
+  return byteArray;
+}
+
+export function byteArrayToLong(byteArray: number[]): number {
+  let value = 0;
+  for (let i = byteArray.length - 1; i >= 0; i -= 1) {
+    value = (value * 256) + byteArray[i];
+  }
+  return value;
+}
+
+export function longToByteArray(long: number): number[] {
+  const byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
+  for (let index = 0; index < byteArray.length; index += 1) {
+    const byte = long & 0xff;
+    byteArray[index] = byte;
+    long = (long - byte) / 256;
+  }
+  return byteArray;
+}
