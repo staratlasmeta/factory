364c8d50e229cd7a3380e69dcd3a8fbe77a7b552
Mike Tourigny <mtourig3@gmail.com>
2021-08-06 01:18:55 -0400

    FEAT: approve, join and leave org updates

--- src/organizations.ts
+++ src/organizations.ts
@@ -86,7 +86,6 @@ export async function getOrganizationOwner(
   return new PublicKey(info.data.slice(81, 113));
 }
 
-
 /**
  * Create initialize player organization info account instruction
  */
@@ -125,7 +124,7 @@ export async function initOrganizationInfoInstruction(
 }
 
 /**
- * Create a player organization instruction
+ * Create a Create Player Organization transaction instruction
  */
  export async function createOrganizationInstruction(
    name: string,
@@ -206,37 +205,95 @@ export async function initOrganizationInfoInstruction(
 }
 
 /**
- * Approve a player to join an organization
- * (for private orgs only)
- * 
- * name: name of organization 
- * ownerKey: owner of organization
- * playerKey: player to approve
- * organizationProgramId: program Id for organizations
+ * Create an Approve Player transaction instruction
  */
-export async function approvePlayer(
+export async function approvePlayerInstruction(
   name: string,
   ownerKey: PublicKey,
   playerKey: PublicKey,
   organizationProgramId: PublicKey,
-) {
+): Promise<TransactionInstruction> {
 
   // Get org/member pdas
   const [playerOrgPda] = await getOrganizationAccount(name, organizationProgramId);
   const [playerMemberPda] = await getPlayerMemberAccount(name, playerKey, organizationProgramId);
 
   // Approve Player
-  const instruction = new TransactionInstruction({
+  return new TransactionInstruction({
       keys: [{pubkey: ownerKey, isSigner: true, isWritable: true},
              {pubkey: playerOrgPda, isSigner: false, isWritable: true},
              {pubkey: playerMemberPda, isSigner: false, isWritable: true}],
       programId: organizationProgramId,
       data: Buffer.from([2])
   });
+}
 
+/**
+ * Approve a player to join an organization
+ * (for private orgs only)
+ * 
+ * name: name of organization 
+ * ownerKeypair: owner of organization - signer
+ * playerKey: player to approve
+ * organizationProgramId: program Id for organizations
+ */
+export async function approvePlayer(
+  connection: Connection,
+  name: string,
+  ownerKeypair: Keypair,
+  playerKey: PublicKey,
+  organizationProgramId: PublicKey,
+): Promise<string> {
+
+  const instruction = await approvePlayerInstruction(
+    name,
+    ownerKeypair.publicKey,
+    playerKey,
+    organizationProgramId
+  );
   const transaction = new Transaction().add(instruction);
+  const txResult = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    ownerKeypair
+  );
+  return txResult;
+}
+
+/**
+ * Create a Join Organization transaction instruction
+ */
+export async function joinOrganizationInstruction(
+  name: string,
+  factionID: number,
+  playerKey: PublicKey,
+  ownerKey: PublicKey,
+  playerIsSigned: boolean,
+  ownerIsSigned: boolean,
+  organizationProgramId: PublicKey,
+  factionEnlstmentProgramId: PublicKey,
+): Promise<TransactionInstruction> {
+
+  // Player faction account needed to confirm the player is in a specific faction
+  const [playerFactionPda] = await getPlayerFactionPDA(playerKey, factionEnlstmentProgramId);
   
-  return transaction;
+  // Get name byte array and org/member pdas
+  const nameByteArray = getOrgNameBytes(name);
+  const [playerOrgPda] = await getOrganizationAccount(name, organizationProgramId);
+  const [playerMemberPda] = await getPlayerMemberAccount(name, playerKey, organizationProgramId);
+  
+  // Join Player Organization
+  return new TransactionInstruction({
+      keys: [{pubkey: playerKey, isSigner: playerIsSigned, isWritable: true},
+             {pubkey: playerFactionPda, isSigner: false, isWritable: true},
+             {pubkey: playerOrgPda, isSigner: false, isWritable: true},
+             {pubkey: playerMemberPda, isSigner: false, isWritable: true},
+             {pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: true},
+             {pubkey: SystemProgram.programId, isSigner: false, isWritable: true},
+             {pubkey: ownerKey, isSigner: ownerIsSigned, isWritable: true}],
+      programId: organizationProgramId,
+      data: Buffer.from([3, ...longToByteArray(factionID), ...nameByteArray])
+  });
 }
 
 /**
@@ -251,52 +308,92 @@ export async function approvePlayer(
  * organizationProgramId: program Id for organizations
  * factionEnlistmentProgramId: program Id for faction enlistment
  */
-export async function joinOrganization(
+ export async function joinOrganization(
+  connection: Connection,
+  name: string,
+  factionID: number,
+  playerKeypair: Keypair = null,
+  ownerKeypair: Keypair = null,
+  playerIsSigned: boolean,
+  ownerIsSigned: boolean,
+  organizationProgramId: PublicKey,
+  factionEnlstmentProgramId: PublicKey,
+): Promise<string> {
+
+  // Get owner from on chain account & confirm matches argument if passed in
+  const ownerPubkey = await getOrganizationOwner(name, connection, organizationProgramId);
+  if (ownerKeypair && ownerPubkey.toBase58() != ownerKeypair.publicKey.toBase58()) {
+    throw 'Unable to join organization, owner account mismatch';
+  }
+
+  const instruction = await joinOrganizationInstruction(
+    name,
+    factionID,
+    playerKeypair.publicKey,
+    ownerPubkey,
+    playerIsSigned,
+    ownerIsSigned,
+    organizationProgramId,
+    factionEnlstmentProgramId
+  );
+  
+  // Player OR Organization Owner can join a player to the organization
+  let signerKeypair: Keypair;
+  if (playerIsSigned && !ownerIsSigned) {
+    signerKeypair = playerKeypair;
+  } else if (!playerIsSigned && ownerIsSigned) {
+    signerKeypair = ownerKeypair;
+  } else {
+    throw 'Invalid mismatch: only player or owner can sign transaction';
+  }
+
+  const transaction = new Transaction().add(instruction);
+  const txResult = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    signerKeypair
+  );
+  return txResult;
+}
+
+/**
+ * Create a Leave Organization transaction instruction
+ */
+export async function leaveOrganizationInstruction(
   name: string,
   factionID: number,
   playerKey: PublicKey,
+  ownerKey: PublicKey,
   playerIsSigned: boolean,
   ownerIsSigned: boolean,
-  connection: Connection,
-  organizationProgramId: PublicKey,
-  factionEnlstmentProgramId: PublicKey,
-): Promise<Transaction> {
+  organizationProgramId: PublicKey
+): Promise<TransactionInstruction> {
 
-  // Player faction account needed to confirm the player is in a specific faction
-  const [playerFactionPda] = await getPlayerFactionPDA(playerKey, factionEnlstmentProgramId);
-  
   // Get name byte array and org/member pdas
   const nameByteArray = getOrgNameBytes(name);
   const [playerOrgPda] = await getOrganizationAccount(name, organizationProgramId);
   const [playerMemberPda] = await getPlayerMemberAccount(name, playerKey, organizationProgramId);
-  
-  // Get owner from on chain account
-  const ownerPubkey = await getOrganizationOwner(name, connection, organizationProgramId);
 
-  // Join Player Organization
-  const instruction = new TransactionInstruction({
+  // Leave Organization
+  return new TransactionInstruction({
       keys: [{pubkey: playerKey, isSigner: playerIsSigned, isWritable: true},
-             {pubkey: playerFactionPda, isSigner: false, isWritable: true},
+             {pubkey: ownerKey, isSigner: ownerIsSigned, isWritable: true},
              {pubkey: playerOrgPda, isSigner: false, isWritable: true},
-             {pubkey: playerMemberPda, isSigner: false, isWritable: true},
-             {pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: true},
-             {pubkey: SystemProgram.programId, isSigner: false, isWritable: true},
-             {pubkey: ownerPubkey, isSigner: ownerIsSigned, isWritable: true}],
+             {pubkey: playerMemberPda, isSigner: false, isWritable: true}],
       programId: organizationProgramId,
-      data: Buffer.from([3, ...longToByteArray(factionID), ...nameByteArray])
+      data: Buffer.from([4, ...longToByteArray(factionID), ...nameByteArray])
   });
-
-  const transaction = new Transaction().add(instruction);
-
-  return transaction;
 }
 
 /**
  * Leave an organization
  * 
+ * (or Kick a player from the organization via Owner)
+ * 
  * name: name of organization 
  * factionID: factionID of organization 
  * playerKey: player to join organization
+ * ownerKey: owner of organization - can kick player from organization
  * playerIsSigned: if player joins themselves, player needs to sign 
  * ownerIsSigned: if owner joins a player, owner needs to sign 
  * connection: Solana Connection
@@ -305,31 +402,45 @@ export async function joinOrganization(
 export async function leaveOrganization(
   name: string,
   factionID: number,
-  playerKey: PublicKey,
+  playerKeypair: Keypair = null,
+  ownerKeypair: Keypair = null,
   playerIsSigned: boolean,
   ownerIsSigned: boolean,
   connection: Connection,
   organizationProgramId: PublicKey
-): Promise<Transaction> {
+): Promise<string> {
 
-  // Get name byte array and org/member pdas
-  const nameByteArray = getOrgNameBytes(name);
-  const [playerOrgPda] = await getOrganizationAccount(name, organizationProgramId);
-  const [playerMemberPda] = await getPlayerMemberAccount(name, playerKey, organizationProgramId);
-
-  // Get owner from on chain account
+  // Get owner from on chain account & confirm matches argument if passed in
   const ownerPubkey = await getOrganizationOwner(name, connection, organizationProgramId);
+  if (ownerKeypair && ownerPubkey.toBase58() != ownerKeypair.publicKey.toBase58()) {
+    throw 'Unable to leave organization, owner account mismatch';
+  }
 
-  // Leave Organization
-  const instruction = new TransactionInstruction({
-      keys: [{pubkey: playerKey, isSigner: playerIsSigned, isWritable: true},
-             {pubkey: ownerPubkey, isSigner: ownerIsSigned, isWritable: true},
-             {pubkey: playerOrgPda, isSigner: false, isWritable: true},
-             {pubkey: playerMemberPda, isSigner: false, isWritable: true}],
-      programId: organizationProgramId,
-      data: Buffer.from([4, ...longToByteArray(factionID), ...nameByteArray])
-  });
+  const instruction = await leaveOrganizationInstruction(
+    name,
+    factionID,
+    playerKeypair.publicKey,
+    ownerPubkey,
+    playerIsSigned,
+    ownerIsSigned,
+    organizationProgramId
+  );
+
+  // Player OR Organization Owner can join a player to the organization
+  let signerKeypair: Keypair;
+  if (playerIsSigned && !ownerIsSigned) {
+    signerKeypair = playerKeypair;
+  } else if (!playerIsSigned && ownerIsSigned) {
+    signerKeypair = ownerKeypair;
+  } else {
+    throw 'Invalid mismatch: only player or owner can sign transaction';
+  }
+
   const transaction = new Transaction().add(instruction);
-
-  return transaction;
+  const txResult = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    signerKeypair
+  );
+  return txResult;
 }
