0a5bf027e439af4e312cf7344a161b8760aab881
Bryan Zierk <bryan.zierk@gmail.com>
2021-10-14 13:33:57 -0600

    FEAT: Continued helper functions.

--- src/util/scoreHelpers.ts
+++ src/util/scoreHelpers.ts
@@ -1,582 +1,110 @@
 import {
+  Provider,
     web3
 } from '@project-serum/anchor'
+import { ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
-const baseIdl: unknown = {
-  "version": "0.0.0",
-  "name": "score",
-  "instructions": [
-    {
-      "name": "processInitializeScoreVars",
-      "accounts": [
-        {
-          "name": "updateAuthorityAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "scoreVarsAccount",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "bump",
-          "type": "u8"
-        }
-      ]
-    },
-    {
-      "name": "processInitialDeposit",
-      "accounts": [
-        {
-          "name": "playerAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "shipStakingAccount",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "escrowAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipTokenAccountSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "shipTokenAccountEscrow",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "bump",
-          "type": "u8"
-        },
-        {
-          "name": "shipQuantity",
-          "type": "u64"
-        }
-      ]
-    },
-    {
-      "name": "processRefuel",
-      "accounts": [
-        {
-          "name": "playerAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "shipStakingAccount",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "escrowAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "fuelMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "fuelTokenAccountSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "fuelTokenAccountEscrow",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "fuelQuantity",
-          "type": "u64"
-        }
-      ]
-    },
-    {
-      "name": "processRefeed",
-      "accounts": [
-        {
-          "name": "playerAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "shipStakingAccount",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "escrowAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "foodMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "foodTokenAccountSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "foodTokenAccountEscrow",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "foodQuantity",
-          "type": "u64"
-        }
-      ]
-    },
-    {
-      "name": "processRearm",
-      "accounts": [
-        {
-          "name": "playerAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "shipStakingAccount",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "escrowAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "armsMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "armsTokenAccountSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "armsTokenAccountEscrow",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "armsQuantity",
-          "type": "u64"
-        }
-      ]
-    },
-    {
-      "name": "processRepair",
-      "accounts": [
-        {
-          "name": "playerAccount",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "shipStakingAccount",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "escrowAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "shipMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "toolkitMint",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "toolkitTokenAccountSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "toolkitTokenAccountEscrow",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "toolkitQuantity",
-          "type": "u64"
-        }
-      ]
-    }
-  ],
-  "accounts": [
-    {
-      "name": "ShipStaking",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "owner",
-            "type": "publicKey"
-          },
-          {
-            "name": "shipMint",
-            "type": "publicKey"
-          },
-          {
-            "name": "shipQuantityInEscrow",
-            "type": "u64"
-          },
-          {
-            "name": "fuelQuantityInEscrow",
-            "type": "u64"
-          },
-          {
-            "name": "foodQuantityInEscrow",
-            "type": "u64"
-          },
-          {
-            "name": "armsQuantityInEscrow",
-            "type": "u64"
-          },
-          {
-            "name": "toolkitQuantityInEscrow",
-            "type": "u64"
-          },
-          {
-            "name": "fuelCurrentCapacity",
-            "type": "u64"
-          },
-          {
-            "name": "foodCurrentCapacity",
-            "type": "u64"
-          },
-          {
-            "name": "armsCurrentCapacity",
-            "type": "u64"
-          },
-          {
-            "name": "toolkitCurrentCapacity",
-            "type": "u64"
-          },
-          {
-            "name": "stakedAtTimestamp",
-            "type": "i64"
-          },
-          {
-            "name": "fueledAtTimestamp",
-            "type": "i64"
-          },
-          {
-            "name": "fedAtTimestamp",
-            "type": "i64"
-          },
-          {
-            "name": "armedAtTimestamp",
-            "type": "i64"
-          },
-          {
-            "name": "repairedAtTimestamp",
-            "type": "i64"
-          },
-          {
-            "name": "totalTimeStaked",
-            "type": "u64"
-          }
-        ]
-      }
-    },
-    {
-      "name": "ScoreVars",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "updateAuthority",
-            "type": "publicKey"
-          },
-          {
-            "name": "fuelBurnRate",
-            "type": "u16"
-          },
-          {
-            "name": "foodBurnRate",
-            "type": "u16"
-          },
-          {
-            "name": "armsBurnRate",
-            "type": "u16"
-          },
-          {
-            "name": "toolkitBurnRate",
-            "type": "u16"
-          },
-          {
-            "name": "pearceX4RewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "opalJetRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "pearceX5RewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "opalJetjetRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "vzusOpodRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "filmbulByosRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "calicoCompaktRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "ogrikaThripidRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "calicoGuardianRewardRate",
-            "type": "u64"
-          },
-          {
-            "name": "padding",
-            "type": {
-              "array": [
-                "u64",
-                64
-              ]
-            }
-          }
-        ]
-      }
-    }
-  ],
-  "metadata": {
-    "address": '' 
-  }
-}
-
-export function getIDL(
-  programId: web3.PublicKey,
-): unknown {
-  const _tmp = baseIdl;
-  _tmp['metadata']['address'] = programId.toBase58();
-  return _tmp;
-}
-
-export async function getScoreVarsAccount(
-    programId: web3.PublicKey
-): Promise<[web3.PublicKey, number]> {
-    return web3.PublicKey.findProgramAddress([
-        Buffer.from('SCORE_VARS')],
-        programId,
-        );
-}
-
-export async function getScoreEscrowAccount(
-    programId: web3.PublicKey,
-    shipMint: web3.PublicKey,
-    resourceMint: web3.PublicKey,
-    user: web3.PublicKey):
-    Promise<[web3.PublicKey, number]> {
-        let seeds = [
-            Buffer.from('SCORE_ESCROW'),
-            user.toBuffer(),
-            shipMint.toBuffer()
-        ];
-        if (resourceMint !== null) {
-            seeds.push(resourceMint.toBuffer());
-        }
-        return web3.PublicKey.findProgramAddress(
-            seeds,
-            programId
-        );
-    }
-
-export async function getScoreAuthEscrowAccount(
-    programId: web3.PublicKey,
-    shipMint: web3.PublicKey,
-    user: web3.PublicKey):
-    Promise<[web3.PublicKey, number]> {
-        return web3.PublicKey.findProgramAddress([
-            Buffer.from('SCORE_ESCROW_AUTHORITY'),
-            shipMint.toBuffer(),
-            user.toBuffer()],
-            programId,
-        );
-    }
-
-export async function getShipStakingAccount(
-  programId: web3.PublicKey,
-  assetMint: web3.PublicKey,
-  user: web3.PublicKey
+export async function getAtaForMint(
+  mint: web3.PublicKey,
+  buyer: web3.PublicKey
 ): Promise<[web3.PublicKey, number]> {
   return web3.PublicKey.findProgramAddress([
-    Buffer.from('SCORE_INFO'),
-    assetMint.toBuffer(),
-    user.toBuffer()],
-    programId,
+    buyer.toBuffer(),
+    TOKEN_PROGRAM_ID.toBuffer(),
+    mint.toBuffer()],
+    ASSOCIATED_TOKEN_PROGRAM_ID,
   );
+};
+
+export async function createATokenAccount(
+  mint: web3.PublicKey,
+  playerPublicKey: web3.PublicKey
+): Promise<[web3.Keypair]> {
+  const [aTokenAccount] = await getAtaForMint(mint, playerPublicKey);
+  const tx = new web3.Transaction();
+  tx.add()
+}
+
+function createAssociatedTokenAccountInstruction(
+  associatedTokenAccount: web3.PublicKey,
+  payer: web3.PublicKey,
+  walletAddress: web3.PublicKey,
+  splTokenMintAddress: web3.PublicKey
+): Promise<[web3.TransactionInstruction]> {
+  const keys = [
+    {
+      pubkey: payer,
+      isSigner: true,
+      isWritable: true,
+    },
+    {
+      pubkey: associatedTokenAccount,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: walletAddress,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: splTokenMintAddress,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: web3.SystemProgram.programId,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: TOKEN_PROGRAM_ID,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: web3.SYSVAR_RENT_PUBKEY,
+      isSigner: false,
+      isWritable: false,
+    },
+  ];
+  let txInstruction = new web3.TransactionInstruction(
+    {
+      keys: keys,
+      programId: ASSOCIATED_TOKEN_PROGRAM_ID,
+      data: Buffer.from([]),
+    }
+  );
+
+  return txInstruction
+}
+
+export async function mintTokens(
+  playerPublicKey: web3.PublicKey,
+  provider: Provider,
+  mint: web3.PublicKey,
+  associatedTokenAccount: web3.PublicKey,
+  amount: number
+): Promise<web3.TransactionSignature> {
+  const tx = new web3.Transaction();
+  tx.add(Token.createMintToInstruction(
+    TOKEN_PROGRAM_ID,
+    mint,
+    associatedTokenAccount,
+    playerPublicKey,
+    [],
+    amount,
+  ));
+  const txid = await provider.send(tx);
+  return txid;
+}
+
+export async function createMint(
+  account: web3.PublicKey,
+  provider: Provider,
+  decimals: number,
+): Promise<web3.PublicKey> {
+  const tx = new web3.Transaction();
+
+
 }
