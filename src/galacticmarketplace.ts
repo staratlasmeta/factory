bc529e23ccb911adba7208c408a1cbfc6a3251c0
Bryan Zierk <bryan.zierk@gmail.com>
2022-01-25 11:28:59 -0700

    Add fn to perform exchange

--- src/galacticmarketplace.ts
+++ src/galacticmarketplace.ts
@@ -8,6 +8,7 @@ import {
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { baseIdl } from "./util/gmIdl";
 import { createAccountInstruction } from "./util";
+import { TransactionInstruction } from "@solana/web3.js";
 
 /**
  * Returns the IDL for the Galactic Marketplace program with provided program ID stored in metadata.
@@ -54,30 +55,63 @@ export async function getOfferVaultAuth(
 }
 
 /**
- * Returns the public key and bump seed for an offer account specific to the user's public key, deposit token account, and receive token account.
+ * Creates an instruction which exchanges tokens between offer initializer and offer taker to satisfy the exchange detailed in offerAccount
  *
- * @param programId
- * @param playerPublicKey
- * @param initializerDepositTokenAccount
- * @param initializerReceiveTokenAccount
+ * @param connection
+ * @param offerTakerMainAccount - PublicKey of the offer taker
+ * @param offerTakerDepositTokenAccount - Token account for token being sent by taker
+ * @param offerTakerReceiveTokenAccount - Token account for token being received by taker
+ * @param offerInitializer - PublicKey of the offer initializer
+ * @param offerAccount - an initialized offerAccount
+ * @param programId - Deployed program ID for GM program
  */
-export async function getOfferAccount(
+export async function createExchangeInstruction(
+    connection: web3.Connection,
+    offerTakerMainAccount: web3.PublicKey,
+    offerTakerDepositTokenAccount: web3.PublicKey,
+    offerTakerReceiveTokenAccount: web3.PublicKey,
+    offerInitializer: web3.PublicKey,
+    offerAccount: web3.PublicKey,
     programId: web3.PublicKey,
-    playerPublicKey: web3.PublicKey,
-    initializerDepositTokenAccount: web3.PublicKey,
-    initializerReceiveTokenAccount: web3.PublicKey,
-): Promise<[web3.PublicKey, number]> {
-    return web3.PublicKey.findProgramAddress(
-        [
-            Buffer.from("offer-account"),
-            playerPublicKey.toBuffer(),
-            initializerDepositTokenAccount.toBuffer(),
-            initializerReceiveTokenAccount.toBuffer()
-        ],
-        programId,
+): Promise<TransactionInstruction> {
+    const idl = getGmIDL(programId);
+    const provider = new Provider(connection, null, null);
+    const program = new Program(idl as Idl, programId, provider);
+
+    const [offerVaultAccount, _offerVaultBump] = await getOfferVault(programId);
+    const [offerVaultAuthority, _offerVaultAuthBump] = await getOfferVaultAuth(programId);
+
+    const ix = program.instruction.processExchange(
+        {
+            accounts: {
+                offerTaker: offerTakerMainAccount,
+                offerTakerDepositTokenAccount,
+                offerTakerReceiveTokenAccount,
+                offerInitializer,
+                offerAccount,
+                offerVaultAccount,
+                offerVaultAuthority,
+                tokenProgram: TOKEN_PROGRAM_ID,
+            }
+        }
     );
+
+    return ix;
 }
 
+/**
+ * Creates an instruction which creates an offer to exchange offerAmount of DepositToken for takerAmount of ReceiveToken
+ *
+ * @param connection
+ * @param initializerPublicKey - PublicKey of offer initializer
+ * @param initializerDepositTokenAccount - Token account for token being offered
+ * @param initializerReceiveTokenAccount - Token account for token to be received
+ * @param offerAccount - an initialized offerAccount
+ * @param offerAmount - number of tokens to be transferred to offer taker
+ * @param takerAmount - number of tokens to be transferred to offer initializer
+ * @param offerTokenMint - Mint address of token being offered
+ * @param programId - Deployed program ID for GM program
+ */
 export async function createInitializeOfferInstruction(
     connection: web3.Connection,
     initializerPublicKey: web3.PublicKey,
--- src/util/gmHelper.ts
+++ src/util/gmHelper.ts
@@ -1,17 +1,27 @@
 import {
-    BN,
     Idl,
     Program,
     Provider,
     web3
 } from "@project-serum/anchor";
-import { Signer, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
-import { getGmIDL, getOfferAccount } from "..";
+import { SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
+import { getGmIDL } from "..";
 
+/**
+ * Creates an instruction to initialize an offer account which can be passed into an initialize offer instruction.
+ *
+ * Must add to an existing web3.Transaction. Transaction recentBlockhash and feePayer must be defined.
+ * Must call ".partialsign(offerAccount)" before sending transaction containing this instruction or you will receive signature verification errors.
+ *
+ * @param connection
+ * @param payer - PublicKey of offer initializer
+ * @param offerAccount - Keypair for offer account, can be obtained with "anchor.web3.Keypair.generate()"
+ * @param programId - Deployed program ID for GM program
+ */
 export async function createAccountInstruction(
     connection: web3.Connection,
     payer: web3.PublicKey,
-    signer: web3.Keypair,
+    offerAccount: web3.Keypair,
     programId: web3.PublicKey,
 ): Promise<TransactionInstruction> {
     const idl = getGmIDL(programId);
@@ -20,7 +30,7 @@ export async function createAccountInstruction(
 
     return SystemProgram.createAccount({
         fromPubkey: payer,
-        newAccountPubkey: signer.publicKey,
+        newAccountPubkey: offerAccount.publicKey,
         space: program.account.offerAccount.size,
         lamports: await connection.getMinimumBalanceForRentExemption(
             program.account.offerAccount.size
@@ -29,6 +39,18 @@ export async function createAccountInstruction(
     });
 }
 
+/**
+ * Creates a transaction to initialize an offer account which can be passed into an initialize offer instruction.
+ *
+ * Can ".add()" additional instructions to this transaction.
+ *
+ * Must call ".partialsign(offerAccount)" before sending this transaction or you will receive signature verification errors.
+ *
+ * @param connection
+ * @param payer - PublicKey of offer initializer
+ * @param offerAccount - Keypair for offer account, can be obtained with "anchor.web3.Keypair.generate()"
+ * @param programId - Deployed program ID for GM program
+ */
 export async function createAccountTransaction(
     connection: web3.Connection,
     payer: web3.PublicKey,
@@ -54,7 +76,6 @@ export async function createAccountTransaction(
         ),
         programId
     }))
-    tx.partialSign(offerAccount);
 
     return tx;
 }
