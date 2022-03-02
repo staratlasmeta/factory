37d0b96f1b72a2e7207a1df0a405860ad2ccff16
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-03-02 17:07:53 +0300

    Add more types

--- src/types/xp_accounts.ts
+++ src/types/xp_accounts.ts
@@ -1,5 +1,6 @@
-import type { AnchorTypes } from '@saberhq/anchor-contrib';
+import { PublicKey } from '@solana/web3.js';
 import { IDL as PROGRAM_IDL } from './xp_program';
+import type { AnchorTypes } from '@saberhq/anchor-contrib';
 
 export type XpTypes = AnchorTypes<typeof PROGRAM_IDL>;
 type Accounts = XpTypes['Accounts'];
@@ -8,3 +9,18 @@ export type UserXpAccount = Accounts['userXpAccount'];
 export type XpAccount = Accounts['xpAccount'];
 export type XpModifier = Accounts['xpModifier'];
 export type XpVars = Accounts['xpVars'];
+
+export interface UserXpAccountItem {
+  publicKey: PublicKey;
+  account: UserXpAccount;
+}
+
+export interface XpModifierItem {
+  publicKey: PublicKey;
+  account: XpModifier;
+}
+
+export interface XpAccountItem {
+  publicKey: PublicKey;
+  account: XpAccount;
+}
