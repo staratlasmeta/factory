86ef6e702f39be744e1c1688d3eec297d1b04421
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-07-19 17:25:40 +0300

    Export PointsStore

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export * from './score';
 export * from './types';
 export * from './util/scoreHelpers';
 export * from './points';
+export * from './points-store';
 export * from './util/gmHelper';
 export * from './util';
 
