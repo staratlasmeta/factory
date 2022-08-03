bf1dd58c60a7d8d66e91e7da8bbdda7c3c5a4757
Mike Tourigny <mtourig3@gmail.com>
2022-08-03 17:10:00 -0400

    FEAT: updated with proxy-rewarder fetch accounts fns

--- src/dao/idl/index.ts
+++ src/dao/idl/index.ts
@@ -1,2 +1,3 @@
 export * from './snapshotsIdl';
 export * from './lockedVoterIdl';
+export * from './proxyRewarderIdl';
--- src/dao/index.js
+++ src/dao/index.js
@@ -1,2 +1,3 @@
 export * from './snapshots';
 export * from './locker';
+export * from './proxy-rewarder';
