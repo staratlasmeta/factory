293a305ae5347d4150dfb971eaf7e4c4b64d236a
Bryan Zierk <bryan.zierk@gmail.com>
2022-08-30 10:13:53 -0600

    fix(GM) - Make API changes non-breaking
    - Cancel takes an optional payer parameter which allows authority
    wallet to sign/pay for cancel tx. Defaults to order initializer if not
    provided
    - Exchange temporarily takes optional params for staking program

--- src/marketplace/instruction_builders/exchangeOrder.ts
+++ src/marketplace/instruction_builders/exchangeOrder.ts
@@ -1,4 +1,4 @@
-import { BN, web3 } from '@project-serum/anchor';
+import { AnchorError, BN, web3 } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 import { getMarketplaceProgram } from '../utils';
 import { getOrderVault } from '../pda_getters';
@@ -20,9 +20,9 @@ export interface ExchangeOrderParams extends BaseParams {
     currencyMint: PublicKey,
     orderInitializer: PublicKey,
     saVault: PublicKey,
-    stakingProgramId: web3.PublicKey,
-    registeredStake: web3.PublicKey,
-    stakingAccount: web3.PublicKey,
+    stakingProgramId?: web3.PublicKey,
+    registeredStake?: web3.PublicKey,
+    stakingAccount?: web3.PublicKey,
 }
 
 /**
@@ -66,6 +66,10 @@ export async function createExchangeInstruction ({
     signers: [],
     instructions: [],
   };
+  // TODO - Remove dummy values when staking launches
+  stakingProgramId = (web3.Keypair.generate()).publicKey;
+  registeredStake = (web3.Keypair.generate()).publicKey;
+  stakingAccount = (web3.Keypair.generate()).publicKey;
 
   // Get order account and info
   const initializerDepositMint =
