47db4087585eb16184912d855dd1ccc504cb0704
Bryan Zierk <bryan.zierk@gmail.com>
2022-08-30 11:17:48 -0600

    Make register stake a permissioned ix
    - Update IDL
    - Add getters for StakingVars account
    - Update register stake ix builder to take authority parm

---
+++ src/atlas-staking/instruction_builders/initializeStaking.ts
@@ -0,0 +1,36 @@
+import { web3 } from '@project-serum/anchor';
+import { FactoryReturn } from '../../types';
+import { getStakingProgram } from '../utils';
+import { BaseStakingParams } from './baseParams';
+
+export interface InitStakingParameters extends BaseStakingParams {
+  updateAuthorityAccount: web3.PublicKey;
+}
+
+/**
+ * Returns an instruction which initializes a staking program with a specified update authority
+ *
+ * @param connection
+ * @param updateAuthorityAccount - Public key of account which will be set as the program's upddate authority
+ * @param programId - Deployed program ID for staking program
+ */
+export async function createInitializeStakingInstruction({
+  connection,
+  updateAuthorityAccount,
+  programId,
+}: InitStakingParameters): Promise<FactoryReturn> {
+  const program = getStakingProgram({ connection, programId });
+
+  const ixSet: FactoryReturn = {
+    signers: [],
+    instructions: [],
+  };
+
+  const ix = await program.methods
+    .initializeStaking()
+    .accounts({ updateAuthorityAccount })
+    .instruction();
+
+  ixSet.instructions.push(ix);
+  return ixSet;
+}
